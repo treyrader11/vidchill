6580e0804173d9aec874f49590fc2a035b16dac5
‘Trey <treyrdr09@gmail.com>
2023-10-31 12:40:51 -0500

    used came case prop naming

--- src/Components/Icons/GreenIcons.tsx
+++ src/Components/Icons/GreenIcons.tsx
@@ -103,7 +103,7 @@ export function GreenUserCheck(props: React.SVGProps<SVGSVGElement>) {
       <path
         d="M28 31.5H23.5C22.1044 31.5 21.4067 31.5 20.8389 31.6722C19.5605 32.06 18.56 33.0605 18.1722 34.3389C18 34.9067 18 35.6044 18 37M32 34L34 36L38 32M30.5 23.5C30.5 25.9853 28.4853 28 26 28C23.5147 28 21.5 25.9853 21.5 23.5C21.5 21.0147 23.5147 19 26 19C28.4853 19 30.5 21.0147 30.5 23.5Z"
         stroke="#11999E"
-        stroke-width="2"
+        strokeWidth="2"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
